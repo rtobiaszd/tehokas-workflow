31428a29d4a66506d687170cd5360183d02e1661
Roberto Tobias <roberto.dalchau@slmandic.edu.br>
2026-01-19 02:10:25 -0300

    try route fix

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\WebhookController;
+use App\Http\Middleware\IdentifyTenant;
+use App\Http\Middleware\ValidateWebhookToken;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/webhooks/workflows', [WebhookController::class, 'handle'])
-   >middleware([
+    ->middleware([
         ValidateWebhookToken::class,
         IdentifyTenant::class,
     ]);
